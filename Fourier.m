2ba7f4e8276d7c481b3053d765d540661d49c369
Sydney Dimmock <sd14814@my.bristol.ac.uk>
2017-04-10 18:11:30 +0100

    Show how to reconstruct an image using just phase information, updated .gitignore for matlab temporary files.

--- Fourier.m
+++ Fourier.m
@@ -1,9 +1,19 @@
-v1 = imread('characters/V1.GIF'); s1 = imread('characters/S1.GIF'); t1 = imread('characters/T1.GIF');
+
+%#just change the file path to load the image.
+v1 = imread('characters/V2.GIF'); s1 = imread('characters/S2.GIF'); t1 = imread('characters/T2.GIF');
 ftv1 = fftshift( fft2(double(v1)) ); fts1 = fftshift( fft2(double(s1)) ); ftt1 = fftshift( fft2(double(t1)) );
 
-Magq = abs(ftv1);
+%%plotting the phase%%
+V1_Phase = angle(ftv1); imagesc(V1_Phase);    % #angle() returns matrix of radians, plotted here
+x = exp(i*V1_Phase);                          % #inverse fft :: complex -> double so convert phase into complex plane using eulers formula e^jp = cos(x) + jsin(x)
+Reconstruct_Just_Phase = ifft2(ifftshift(x)); % # Perform the inverse fft
+imshow(Reconstruct_Just_Phase) %It shows outline but is hard to see
 
-figure('Name','Fourier space of V');
-imagesc(log(abs(ftv1)+1)); figure('Name','Fourier space of S');
-imagesc(log(abs(fts1)+1)); figure('Name','Fourier space of T');
-imagesc(log(abs(ftt1)+1));
+%%Magnitude%%
+V1_Mag = abs(ftv1);
+
+%%%Figures for each letter, uncomment to show%%%
+%figure('Name','Fourier space of V');
+%imagesc(log(abs(ftv1)+1)); figure('Name','Fourier space of S');
+%imagesc(log(abs(fts1)+1)); figure('Name','Fourier space of T');
+%imagesc(log(abs(ftt1)+1));
