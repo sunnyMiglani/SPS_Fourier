4e70d855591f5ff2015a04cfce0ef97455f90eb9
Sydney Dimmock <sd14814@my.bristol.ac.uk>
2017-04-10 15:00:01 +0100

    Fourier spaces of each letter

--- Fourier.m
+++ Fourier.m
@@ -1,6 +1,9 @@
-v1 = imread('characters/V1.GIF');
-ftv1 = fftshift( fft2(double(v1)) );
+v1 = imread('characters/V1.GIF'); s1 = imread('characters/S1.GIF'); t1 = imread('characters/T1.GIF');
+ftv1 = fftshift( fft2(double(v1)) ); fts1 = fftshift( fft2(double(s1)) ); ftt1 = fftshift( fft2(double(t1)) );
 
 Magq = abs(ftv1);
 
-imagesc(log(abs(ftv1)+1))
+figure('Name','Fourier space of V');
+imagesc(log(abs(ftv1)+1)); figure('Name','Fourier space of S');
+imagesc(log(abs(fts1)+1)); figure('Name','Fourier space of T');
+imagesc(log(abs(ftt1)+1));
