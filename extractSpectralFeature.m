a362a4f15ead78ec872a9b7af737a6121d3524d6
Sunny Miglani <sunny.miglani.2015@my.bristol.ac.uk>
2017-04-26 17:33:49 +0100

    changed it to using 2D arrays so we can save both the features

--- extractSpectralFeature.m
+++ extractSpectralFeature.m
@@ -5,6 +5,12 @@ V_files = dir('characters/Vs/');
 V_Path = 'characters/Vs/';
 T_files = dir('characters/Ts/');
 T_Path = 'characters/Ts/';
+
+% The reason i starts from 3 is because dir() returns ".", ".." as the
+% possible options in the directories, and this throws errors
+% this is also why we use S(i-2,...) to avoid empty spaces in the S,V,T
+% arrays
+
 for i = 3 : numel(S_files)
     Image = imread(strcat(S_Path,S_files(i).name));                                       %Read the file name
     FFTM  = fftshift( fft2(double(Image)));                                %FFT
@@ -12,8 +18,8 @@ for i = 3 : numel(S_files)
     Box_Power = Sum_Power(abs(Box));                                       %Sum the powers to get the value for this feature
     Ring = Extract_Ring(FFTM, 150, 50);
     Ring_Power = Sum_Power(abs(Ring));
-    S(i) = Box_Power + Ring_Power;                                                      %Store the value in the return vector
-
+    S(i-2,1) = Box_Power;                                                      %Store the value in the return vector
+    S(i-2,2) = Ring_Power;
 end
 for i = 3 : numel(V_files)
 
@@ -23,8 +29,8 @@ for i = 3 : numel(V_files)
     Box_Power = Sum_Power(abs(Box));                                       %Sum the powers to get the value for this feature
     Ring = Extract_Ring(FFTM, 150, 50);
     Ring_Power = Sum_Power(abs(Ring));
-    V(i) = Box_Power + Ring_Power;                                                      %Store the value in the return vector
-
+    V(i-2,1) = Box_Power;                                                      %Store the value in the return vector
+    V(i-2,2) = Ring_Power;
 end
 for i = 3 : numel(T_files)
 
@@ -34,7 +40,8 @@ for i = 3 : numel(T_files)
     Box_Power = Sum_Power(abs(Box));                                       %Sum the powers to get the value for this feature
     Ring = Extract_Ring(FFTM, 150, 50);
     Ring_Power = Sum_Power(abs(Ring));
-    T(i) = Box_Power + Ring_Power;                                                      %Store the value in the return vector
+    T(i-2,1) = Box_Power;                                                      %Store the value in the return vector
+    T(i-2,2) = Ring_Power;
 end
 end
 
