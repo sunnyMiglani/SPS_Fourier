2581700325c7e9493a2cb7c5eaa81f01378f9b6a
Sydney Dimmock <sd14814@my.bristol.ac.uk>
2017-04-26 20:59:44 +0100

    Got spaces for all training data, as it seems we need more examples to pick our regions better.

--- extractSpectralFeature.m
+++ extractSpectralFeature.m
@@ -16,35 +16,37 @@ u0 = 10; u1 = 70; v0 = 240; v1 = 400;
 % arrays
 
 for i = 3 : numel(S_files)
-    Image = imread(strcat(S_Path,S_files(i).name));                                       %Read the file name
+    Image = imread(strcat(S_Path,S_files(i).name));                        %Read the file name
     FFTM  = fftshift( fft2(double(Image)));                                %FFT
-    Box = Extract_Box(FFTM, u0, u1, v0, v1);                             %Get Feature
+    Box = Extract_Box(FFTM, u0, u1, v0, v1);                               %Get Feature
     Box_Power = Sum_Power(abs(Box));                                       %Sum the powers to get the value for this feature
     Ring = Extract_Ring(FFTM, ring_Outer, ring_Inner);
     Ring_Power = Sum_Power(abs(Ring));
-    S(i-2,1) = Box_Power;                                                      %Store the value in the return vector
+    S(i-2,1) = Box_Power;                                                  %Store the value in the return vector
     S(i-2,2) = Ring_Power;
 end
 for i = 3 : numel(V_files)
 
-    Image = imread(strcat(V_Path,V_files(i).name));                                       %Read the file name
+    Image = imread(strcat(V_Path,V_files(i).name));                        %Read the file name
     FFTM  = fftshift( fft2(double(Image)));                                %FFT
-    Box = Extract_Box(FFTM, u0, u1, v0, v1);                             %Get Feature
+    Box = Extract_Box(FFTM, u0, u1, v0, v1);                               %Get Feature
     Box_Power = Sum_Power(abs(Box));                                       %Sum the powers to get the value for this feature
     Ring = Extract_Ring(FFTM, ring_Outer, ring_Inner);
     Ring_Power = Sum_Power(abs(Ring));
-    V(i-2,1) = Box_Power;                                                      %Store the value in the return vector
+    V(i-2,1) = Box_Power;                                                  %Store the value in the return vector
     V(i-2,2) = Ring_Power;
 end
 for i = 3 : numel(T_files)
 
-    Image = imread(strcat(T_Path,T_files(i).name));                                       %Read the file name
+    Image = imread(strcat(T_Path,T_files(i).name));                        %Read the file name
     FFTM  = fftshift( fft2(double(Image)));                                %FFT
-    Box = Extract_Box(FFTM, u0, u1, v0, v1);                             %Get Feature
+        figure
+    imagesc(log(abs(FFTM)+1))
+    Box = Extract_Box(FFTM, u0, u1, v0, v1);                               %Get Feature
     Box_Power = Sum_Power(abs(Box));                                       %Sum the powers to get the value for this feature
     Ring = Extract_Ring(FFTM, ring_Outer, ring_Inner);
     Ring_Power = Sum_Power(abs(Ring));
-    T(i-2,1) = Box_Power;                                                      %Store the value in the return vector
+    T(i-2,1) = Box_Power;                                                  %Store the value in the return vector
     T(i-2,2) = Ring_Power;
 end
 end
