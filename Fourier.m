0b47c2be7f225e6784674bd3f449802cbbfd7198
Sydney Dimmock <sd14814@my.bristol.ac.uk>
2017-04-29 12:51:13 +0100

    removed the weird NN number.

--- Fourier.m
+++ Fourier.m
@@ -90,7 +90,7 @@ zlabel('Ring Values');
 % plot(Cluster_V(:,1),Cluster_V(:,2), 'gd', 'MarkerSize', 7);
 % plot(Cluster_T(:,1),Cluster_T(:,2), 'bd', 'MarkerSize', 7);
 
-voronoi(meanArray(:,1), meanArray(:,2));
+%voronoi(meanArray(:,1), meanArray(:,2));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%% - KNN Classifier - %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -99,7 +99,7 @@ voronoi(meanArray(:,1), meanArray(:,2));
 cell_labels = cellstr([repmat('S', 10,1); repmat('V', 10,1); repmat('T', 10,1)]);
 
 %Fit the KNN classifier
-MdL = fitcknn(Training_Data, cell_labels, 'NumNeighbors', 10, 'Standardize', 1);
+MdL = fitcknn(Training_Data, cell_labels, 'NumNeighbors', 3, 'Standardize', 1);
 
 
 [labels,Test_Data] = test_knn(MdL);
--- extractSpectralFeature.m
+++ extractSpectralFeature.m
@@ -13,14 +13,14 @@ BT_u0 = 1; BT_u1 = 170; BT_v0 = 300; BT_v1 = 340;
 %%Right Box Assumptios
 BR_u0 = 180; BR_u1 = 220; BR_v0 = 395; BR_v1 = 640; 
 %%Sector Assumptions (L)
-thetaL_1 = 10; thetaL_2 =30; radL_in = 150; radL_out = 310;
+thetaL_1 = 10; thetaL_2 = 30; radL_in = 150; radL_out = 310;
 %(R)
 thetaR_1 = 150; thetaR_2 = 170; radR_in = 150; radR_out = 310;
 
     function [ X ] = get_Feature( direct, path )
         for i = 1 : numel(direct)
             Image = imread(strcat(path, direct(i).name));                              %Read the file name
-            FFTM  = log(abs(fftshift( fft2(double(Image))))+1); 
+            FFTM  = abs(fftshift( fft2(double(Image)))); 
             Box_Top = Sum_Power( Extract_Box(FFTM, BT_u0, BT_u1, BT_v0, BT_v1));       %Extract a "Box" spectral feature
             Box_R = Sum_Power(Extract_Box(FFTM, BR_u0, BR_u1, BR_v0, BR_v1));                               
             Ring = Sum_Power(Extract_Ring(FFTM, ring_Outer, ring_Inner));
